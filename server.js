e8f57389185d3dca1a475ed42ca53f7fd8f0bfb8
eletricssd-eng <eletric.ssd@gmail.com>
2026-03-19 23:24:49 -0300

    Update server.js
    fix:  corrigindo porta para render

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ res.json({
 
 });
 
-app.listen(3000,()=>{
-    console.log("API REAL rodando em http://localhost:3000");
+const PORT=process.env.PORT||3000;
+app.listen(PORT,()=>{
+    console.log("Servidor rodando na porta" + PORT);
 });
