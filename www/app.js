f590d437a90828b08fdfd5df073d66881df33171
sidney <eletric.ssd@gmail.com>
2026-04-22 09:00:28 -0300

    config build

--- www/app.js
+++ www/app.js
@@ -6,16 +6,36 @@ document.addEventListener("DOMContentLoaded", () => {
     const el = document.getElementById("resultado");
 
     try {
-      el.innerHTML = "Carregando dados...";
+      el.innerHTML = "Carregando...";
 
       const res = await fetch(API);
-      const texto = await res.text(); // evita erro silencioso
+      const data = await res.json();
 
-      el.innerHTML = texto;
+      const hoje = Object.keys(data.historico).pop();
+      const dia = data.historico[hoje];
+
+      let html = `<h2>📅 ${hoje}</h2>`;
+
+      for (let banca in dia) {
+        if (!dia[banca].length) continue;
+
+        html += `<h3 style="color:yellow">${banca.toUpperCase()}</h3>`;
+
+        dia[banca].forEach(r => {
+          html += `
+            <div style="margin-bottom:10px;">
+              🕐 ${r.horario}<br>
+              ${r.p1} - ${r.p2} - ${r.p3} - ${r.p4} - ${r.p5}
+            </div>
+          `;
+        });
+      }
+
+      el.innerHTML = html;
 
     } catch (erro) {
       el.innerHTML = "Erro ao carregar 😢";
-      console.log("ERRO:", erro);
+      console.log(erro);
     }
   }
 
